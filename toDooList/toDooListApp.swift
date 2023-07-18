d591e26c2b6d414d05cf852b6bf237a5299b18fd
Abigail Lang <abigaillang2772@gmail.com>
2023-07-18 10:21:15 -0700

    Initial Commit

---
+++ toDooList/toDooListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDooListApp.swift
+//  toDooList
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct toDooListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
